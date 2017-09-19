1af87232c36788df8ad905f6c47c22f21d2160ca
Carl-Tang <ytan415@aucklanduni.ac.nz>
2017-09-19 16:22:27 +1200

    Initialized Main Scene and MainController

---
+++ src/application/MainController.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController implements Initializable {
+	@FXML
+	private AnchorPane _mainPane;
+	@FXML
+	private AnchorPane _statisticsPane;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
