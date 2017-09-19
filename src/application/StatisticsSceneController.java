a2a1ef2a07293ee598771a639484a47bf4c014bf
Carl-Tang <ytan415@aucklanduni.ac.nz>
2017-09-19 18:17:01 +1200

    Initialized StatisticsScene and StatisticsSceneController

---
+++ src/application/StatisticsSceneController.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import javafx.scene.control.ListView;
+
+public class StatisticsSceneController implements Initializable {
+	@FXML
+	private Label _scoreLabel;
+	@FXML
+	private ListView _resultListView;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
